e890e247696b60670750be4df7062f754727e63a
ThangHND <yuuwhisky@gmail.com>
2025-04-26 15:56:07 +0700

    CRUD create company

---
+++ 01-java-spring-jobhunter-starter-master/src/main/java/vn/hoidanit/jobhunter/controller/CompanyController.java
@@ -0,0 +1,27 @@
+package vn.hoidanit.jobhunter.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import jakarta.validation.Valid;
+import vn.hoidanit.jobhunter.domain.Company;
+import vn.hoidanit.jobhunter.service.CompanyService;
+
+@Controller
+public class CompanyController {
+    private CompanyService companyService;
+
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @PostMapping("/companies")
+    public ResponseEntity<Company> createCompany(@Valid @RequestBody Company PostManCompany) {
+        Company company = this.companyService.handleCreateCompany(PostManCompany);
+        return ResponseEntity.status(HttpStatus.CREATED).body(company);
+    }
+}
